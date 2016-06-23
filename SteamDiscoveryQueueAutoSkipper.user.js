13335f9a001c28920cc976c3fbf62881556cace4
PotcFdk <PotcFdk@gmx.net>
2016-06-23 20:48:42 +0200

    Add /explore/next to handle error-reloading on that page, too

--- SteamDiscoveryQueueAutoSkipper.user.js
+++ SteamDiscoveryQueueAutoSkipper.user.js
@@ -7,7 +7,9 @@
 // @include     https://store.steampowered.com/app/*
 // @include     http://store.steampowered.com/agecheck/app/*
 // @include     https://store.steampowered.com/agecheck/app/*
-// @version     0.1.2
+// @include     http://store.steampowered.com/explore/next/*
+// @include     https://store.steampowered.com/explore/next/*
+// @version     0.1.3
 // @grant       none
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/SteamDiscoveryQueueAutoSkipper/master/SteamDiscoveryQueueAutoSkipper.user.js
 // @updateURL   https://raw.githubusercontent.com/PotcFdk/SteamDiscoveryQueueAutoSkipper/master/SteamDiscoveryQueueAutoSkipper.meta.js
