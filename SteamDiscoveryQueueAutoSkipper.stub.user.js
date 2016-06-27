bbc57b8635ccfc8620ff261b39cf3d3d70833ba9
PotcFdk <PotcFdk@gmx.net>
2016-06-27 17:38:55 +0200

    Update stub.

--- SteamDiscoveryQueueAutoSkipper.stub.user.js
+++ SteamDiscoveryQueueAutoSkipper.stub.user.js
@@ -5,6 +5,10 @@
 // @description Auto-clicks the "Next in Queue" button in Steam Discovery Queues.
 // @include     http://store.steampowered.com/app/*
 // @include     https://store.steampowered.com/app/*
+// @include     http://store.steampowered.com/agecheck/app/*
+// @include     https://store.steampowered.com/agecheck/app/*
+// @include     http://store.steampowered.com/explore*
+// @include     https://store.steampowered.com/explore*
 // @version     0
 // @grant       none
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/SteamDiscoveryQueueAutoSkipper/master/SteamDiscoveryQueueAutoSkipper.user.js
