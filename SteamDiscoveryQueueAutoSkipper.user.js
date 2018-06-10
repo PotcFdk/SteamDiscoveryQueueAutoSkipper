1273f3cbdeb022c1b40b8eb6bc12faeb4141cc12
PotcFdk <PotcFdk@gmx.net>
2018-06-11 00:29:39 +0200

    Update copyright year to 2018.

--- SteamDiscoveryQueueAutoSkipper.user.js
+++ SteamDiscoveryQueueAutoSkipper.user.js
@@ -16,7 +16,7 @@
 // ==/UserScript==
 
 /*
-	Steam Discovery Queue Auto-Skipper - Copyright (c) PotcFdk, 2015 - 2017
+	Steam Discovery Queue Auto-Skipper - Copyright (c) PotcFdk, 2015 - 2018
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
