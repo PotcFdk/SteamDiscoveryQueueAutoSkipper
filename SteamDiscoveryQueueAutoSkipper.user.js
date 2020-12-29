16b86364b24272dab76a0e4cc93a3da3bee693f7
PotcFdk <PotcFdk@gmx.net>
2020-12-29 16:32:48 +0100

    Convert to @match rules.

--- SteamDiscoveryQueueAutoSkipper.user.js
+++ SteamDiscoveryQueueAutoSkipper.user.js
@@ -3,14 +3,11 @@
 // @name        Steam Discovery Queue Auto-Skipper
 // @namespace   https://github.com/PotcFdk/SteamDiscoveryQueueAutoSkipper
 // @description Auto-clicks the "Next in Queue" button in Steam Discovery Queues.
-// @include     http://store.steampowered.com/
-// @include     https://store.steampowered.com/
-// @include     http://store.steampowered.com/app/*
-// @include     https://store.steampowered.com/app/*
-// @include     http://store.steampowered.com/agecheck/app/*
-// @include     https://store.steampowered.com/agecheck/app/*
-// @include     http://store.steampowered.com/explore*
-// @include     https://store.steampowered.com/explore*
+// @match       *://store.steampowered.com/
+// @match       *://store.steampowered.com/app/*
+// @match       *://store.steampowered.com/agecheck/app/*
+// @match       *://store.steampowered.com/explore*
+// @match       *://store.steampowered.com/points*
 // @version     0.8.0
 // @grant       none
 // @icon        https://raw.githubusercontent.com/PotcFdk/SteamDiscoveryQueueAutoSkipper/master/logo.png
