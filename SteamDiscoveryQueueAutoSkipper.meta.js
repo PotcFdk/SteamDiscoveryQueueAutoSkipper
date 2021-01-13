a2f9d19bdf73195ff6cc43f48f3c0659392c91ab
PotcFdk <PotcFdk@gmx.net>
2021-01-13 18:02:19 +0100

    Remove ItemRewards handling. It does not exist on the page anymore and was erroring. Cleanup ItemRewards related localStorage data. Bump version to v0.10.0.

--- SteamDiscoveryQueueAutoSkipper.meta.js
+++ SteamDiscoveryQueueAutoSkipper.meta.js
@@ -9,7 +9,7 @@
 // @include     https://store.steampowered.com/agecheck/app/*
 // @include     http://store.steampowered.com/explore*
 // @include     https://store.steampowered.com/explore*
-// @version     0.9.1
+// @version     0.10.0
 // @grant       none
 // @icon        https://raw.githubusercontent.com/PotcFdk/SteamDiscoveryQueueAutoSkipper/master/logo.png
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/SteamDiscoveryQueueAutoSkipper/master/SteamDiscoveryQueueAutoSkipper.user.js
