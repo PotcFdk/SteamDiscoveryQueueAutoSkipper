4092ed39ad3d228dd19d7d3e95e3c4085be8f1df
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2022-01-08 10:59:32 +0000

    Auto wishlist discovery queue app

--- SteamDiscoveryQueueAutoSkipper.user.js
+++ SteamDiscoveryQueueAutoSkipper.user.js
@@ -63,6 +63,48 @@ function click (obj)
 function handleQueuePage()
 {
 	var btn = document.getElementsByClassName ("btn_next_in_queue")[0];
+	var category_icons = document.getElementsByClassName('category_icon');
+    
+    if (category_icons) {
+        var is_free = false;
+        var is_dlc = false;
+        
+        var btn_addtocart = document.getElementsByClassName('btn_addtocart');
+        if (btn_addtocart) {
+            for (var j=0; j < btn_addtocart.length; j++) {
+                if (btn_addtocart[j].firstElementChild) {
+                    if (btn_addtocart[j].firstElementChild.href && btn_addtocart[j].firstElementChild.href.match('steam://run/')) {
+                        is_free = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (var i=0; i < category_icons.length; i++) {
+            if (category_icons[i].src && category_icons[i].src.match('ico_dlc')) {
+                is_dlc = true;
+                break;
+            }
+        }
+
+        if (!is_free && !is_dlc) {
+            console.log('Adding to Wishlist.');
+            var wishlist_area = document.getElementById('add_to_wishlist_area');
+            if (wishlist_area && wishlist_area.firstElementChild && wishlist_area.firstElementChild.href && wishlist_area.firstElementChild.href != '' && wishlist_area.firstElementChild.href.indexOf('javascript:AddToWishlist(') === 0) {
+                try {
+                    wishlist_area.firstElementChild.click();
+                    document.getElementsByClassName('queue_btn_active')[0].style.border ='1px solid #999999';
+                } catch (err) {
+                    console.log('Error adding to Wishlist. Try reloading the page.');
+                }
+            } else {
+                console.log('This app is already on your Wishlist or there was an error.');
+            }
+        } else {
+            console.log(is_free ? 'This app is free, skipping!' : 'This dlc is free, skipping!');
+        };
+    }
 
 	if (btn)
 	{
